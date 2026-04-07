f9329a3b7395142f2bb0f51faf7d076055ab3a8a
Hector <tudor.voica06@e-uvt.ro>
2026-04-07 15:45:55 +0300

    Some modifications

--- agent/main.py
+++ agent/main.py
@@ -1,10 +1,16 @@
 import time
+import logging
 
 from collector import collect_metrics
 from device_info import get_device_info
 from sender import send_metrics, send_heartbeat
 from config import load_config
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 def main():
     config = load_config()
@@ -16,8 +22,8 @@ def main():
     heartbeat_interval = config["heartbeat_interval"]
     timeout = config["timeout"]
 
-    print("[INFO] Agent started")
-    print(f"[INFO] Device: {device_info}")
+    logging.info("Agent started")
+    logging.info(f"Device: {device_info}")
 
     last_heartbeat_time = 0
 
@@ -26,12 +32,12 @@ def main():
 
         if current_time - last_heartbeat_time >= heartbeat_interval:
             heartbeat_response = send_heartbeat(base_url, api_key, device_info, timeout)
-            print("[HEARTBEAT]", heartbeat_response)
+            logging.info(f"[HEARTBEAT] {heartbeat_response}")
             last_heartbeat_time = current_time
 
         metrics = collect_metrics()
         metrics_response = send_metrics(base_url, api_key, device_info, metrics, timeout)
-        print("[METRICS]", metrics_response)
+        logging.info(f"[METRICS] {metrics_response}")
 
         time.sleep(metrics_interval)
 
